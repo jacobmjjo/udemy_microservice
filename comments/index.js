01a2124ee34682c7c188457b7f96b1f8c324f5e8
myeongjang <jacobmjjo@gmail.com>
2022-01-30 21:24:21 +0900

    add post function and comment fucntion

--- comments/index.js
+++ comments/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
+
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id]) || []
+    res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts:/id/comments', (req, res) =>{
+app.post('/posts/:id/comments', (req, res) =>{
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
 
