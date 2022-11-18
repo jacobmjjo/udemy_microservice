71686a0601395191eec915054a77df283117cd37
ldcc <jacobmjjo@gmail.com>
2022-11-18 17:04:31 +0900

    completed type script course

--- maps/src/CustomMaps.ts
+++ maps/src/CustomMaps.ts
@@ -1,3 +1,18 @@
+import { User } from './User'
+import { Company} from './Company';
+
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -10,4 +25,21 @@ export class CustomMap {
       }
     });
   }
+
+
+addMarker(mappable:Mappable ):void {
+  const marker = new google.maps.Marker({
+    map: this.googleMap,
+    position: {
+      lat: mappable.location.lat,
+      lng: mappable.location.lng
+    },
+  });
+  marker.addListener('click', () => {
+     const infoWindow = new google.maps.InfoWindow({
+      content: mappable.markerContent()
+     });
+     infoWindow.open(this.googleMap, marker);
+  });
+}
 }
