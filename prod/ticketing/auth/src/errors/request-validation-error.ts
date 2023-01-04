94c7bc5251bde8b8560572bd9f8a6f1a2d6441e4
ldcc <jacobmjjo@gmail.com>
2023-01-05 00:40:02 +0900

    applied abstract error interface

---
+++ prod/ticketing/auth/src/errors/request-validation-error.ts
@@ -0,0 +1,24 @@
+import { ValidationError } from "express-validator";
+
+interface CustomError {
+  statusCode: number;
+  serializeErrors(): {
+    message: string;
+    field: string;
+  }[];
+}
+
+export class RequestValidationError extends Error implements CustomError{
+  statusCode = 400;
+  constructor(public errors: ValidationError[]) {
+    super();
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+  serializeErrors() {
+    return this.errors.map(err => {
+      return {message: err.msg, field: err.param};
+    });
+  }
+  
+}
+
