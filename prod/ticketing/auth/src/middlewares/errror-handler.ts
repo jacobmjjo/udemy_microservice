b4c0b03c97104338a44280dbab374c9161f0bb1f
ldcc <jacobmjjo@gmail.com>
2023-01-05 00:42:34 +0900

    applied serializeErrors to prevent gigantic error case

---
+++ prod/ticketing/auth/src/middlewares/errror-handler.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { RequestValidationError } from '../errors/request-validation-error';
+export const errorHandler = (
+  err: Error, 
+  req: Request, 
+  res:Response, 
+  next: NextFunction
+  ) => {
+    if  (err instanceof RequestValidationError) {
+      // 1st comment to show error
+      //console.log('handling this error as a request validation error');
+      
+      //2nd comment to show error
+      //const formattedErrors = err.errors.map(error => {
+      //  return {message: error.msg, field: error.param};
+      //});
+      return res.status(err.statusCode).send({errors: err.serializeErrors()});
+    }
+
+    if (err instanceof DatabaseConnectionError) {
+      // 1st comment to show error
+      //console.log('handling this error as a db connection error');
+      
+      //2nd comment to show error
+      //return res.status(500).send({ errors: [
+      //  {message: err.reason}
+      //]});
+      return res.status(err.statusCode).send({errors: err.serializeErrors()});
+    }
+    res.status(400).send({
+      erros: [{ message: 'Someting went wrong'}]
+    });
+};
