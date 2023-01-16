61ab4e24fc36676c73a5cdec14e272e7dfa61f74
ldcc <jacobmjjo@gmail.com>
2023-01-16 17:36:40 +0900

    add jwt and cookie session

--- prod/ticketing/auth/src/index.ts
+++ prod/ticketing/auth/src/index.ts
@@ -1,23 +1,49 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser'
+import mongoose from 'mongoose'
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
 import { signoutRouter } from './routes/signout';
 import { errorHandler } from './middlewares/errror-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.all('*',async(req, res) => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDB')
+  } catch (err) {
+  console.error(err);
+  }
 
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000!');
+  console.log('Listening on port 3000!!!!!!');
 });
+};
+
+start();
