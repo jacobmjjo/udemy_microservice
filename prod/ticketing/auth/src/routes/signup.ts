d38823b5709a4fb1a50539d0102a6a2b3b31c70a
ldcc <jacobmjjo@gmail.com>
2023-01-16 17:38:11 +0900

    add jwt and sign of jwt

--- prod/ticketing/auth/src/routes/signup.ts
+++ prod/ticketing/auth/src/routes/signup.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response} from 'express';
 import { body, validationResult } from 'express-validator';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import jwt from 'jsonwebtoken';
+
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 
 const router = express.Router();
@@ -18,20 +21,39 @@ router.post(
       .isLength({ min:4, max:20})
       .withMessage('Password must be between 4 and 20 charaters')
 ], 
-(req: Request, res: Response) => {
-  const errors = validationResult(req);
+async (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array());
-     
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      // console.log('Email in use');
+      // return res.send({});
+      throw new BadRequestError('Email in use')
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, 'asdf');
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt
+    };
+
+    res.status(201).send(user);
   }
-
-
-  console.log('Creating a user...');
-  throw new DatabaseConnectionError();
-  
-  res.send({});
-  
-});
+);
 
 export { router as signupRouter };
