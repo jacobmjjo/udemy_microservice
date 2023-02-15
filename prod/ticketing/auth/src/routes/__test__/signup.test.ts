a080840c38bf27886ec5163355d38cf29b728bae
ldcc <jacobmjjo@gmail.com>
2023-02-15 16:07:51 +0900

    add signup testing scenario

--- prod/ticketing/auth/src/routes/__test__/signup.test.ts
+++ prod/ticketing/auth/src/routes/__test__/signup.test.ts
@@ -32,3 +32,24 @@ it('returns a 400 with missing email and password', async () => {
     password: 'dasdfsaf'
   }).expect(400);
 });
+
+it('disallow duplicate emails', async () => {
+  await request(app).post('/api/users/signup').send({
+    email: 'test@test.com',
+    password: 'password'
+  }).expect(201);
+
+  await request(app).post('/api/users/signup').send({
+    email: 'test@test.com',
+    password: 'password'
+  }).expect(400);
+});
+
+it('sets a cookie after successful signup', async () => {
+  const response = await request(app).post('/api/users/signup').send({
+    email: 'test@test.com',
+    password: 'password'
+  }).expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
